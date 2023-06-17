631a664f9c85e7bbb60b5ed2a05e777daa944c4c
sp3lllz <136860095+sp3lllz@users.noreply.github.com>
2023-06-17 01:18:59 +0100

    upload test.py

---
+++ test.py
@@ -0,0 +1,12 @@
+print('Hello what is your name?')
+name = input()
+print('What is the password?')
+password = input()
+
+if name == 'Patrick':
+	if password == 'Millie':
+        print('Acess Granted')
+    else:
+        print('Incorrect Password')
+else:
+print('Incorrect Username')
